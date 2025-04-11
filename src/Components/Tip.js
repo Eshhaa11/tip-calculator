06546cee6c75925a7f01d1ad15e892920258e2a5
Esha Patel <Patellesha11@gmail.com>
2025-04-11 10:45:12 +0300

    added cont tip

--- src/Components/Tip.js
+++ src/Components/Tip.js
@@ -1,24 +1,24 @@
 import { useState } from "react";
-import "./Tip.css"
+import "./Tip.css";
 
-function Tip ()  {
-    const [purchase, setPurchase] = useState("")
-    const tip = purchase >= 1000 ? Math.floor(purhcase / 1000) * 50 : 0;
-    const total = Number(purchase) + tip;
-    
-    return (
-        <div className="tip-cont">
-            <h1>Tip Calculator</h1>
-            <input 
-            type="number"
-            placeholder="Enter the amount in Ksh"
-            value={purchase}
-            onChange={(e) => setPurchase(e.target.value)}
-        />
-        <p>Tip: KES {tip}</p>
-        <p>Total: KES {isNaN(total) ? 0 : total}</p>
-        </div>
-    )
+function Tip() {
+  const [purchase, setPurchase] = useState("");
+  const tip = purchase >= 1000 ? Math.floor(purchase / 1000) * 50 : 0;
+  const total = Number(purchase) + tip;
+
+  return (
+    <div className="tip-cont">
+      <h1>Tip Calculator</h1>
+      <input
+        type="number"
+        placeholder="Enter the amount in Ksh"
+        value={purchase}
+        onChange={(e) => setPurchase(e.target.value)}
+      />
+      <p>Tip: KES {tip}</p>
+      <p>Total: KES {isNaN(total) ? 0 : total}</p>
+    </div>
+  );
 }
 
 export default Tip;
