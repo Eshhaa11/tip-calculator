0d00d2bf40d7ee375763cdaba9f02d4a4e232bfb
Esha Patel <Patellesha11@gmail.com>
2025-04-11 10:32:47 +0300

    added const tip

--- src/Components/Tip.js
+++ src/Components/Tip.js
@@ -3,6 +3,7 @@ import "./Tip.css"
 
 function Tip ()  {
     const [purchase, setPurchase] = useState("")
+    const tip = purchase >= 1000 ? Math.floor(purhcase / 1000) * 50 : 0; 
     return (
         <div>
             <h1>Tip Calculator</h1>
